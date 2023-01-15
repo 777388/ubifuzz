c5a2eb168ba4138f43a6c821a78c8767c1698ab7
777388 <96343159+777388@users.noreply.github.com>
2023-01-15 15:16:11 -0800

    Update ubifuzz.py

--- ubifuzz.py
+++ ubifuzz.py
@@ -7,16 +7,20 @@ combinations = []
 for i in range(256):
     binary = format(i, '08b')
     if "00" in binary and binary != "11111111":
-        encoded = urllib.parse.quote(binary) # URL encode the binary string
+        dec = int(binary, 2) # convert binary to decimal
+        char = chr(dec) # convert decimal to utf character
+        encoded = urllib.parse.quote(char) # URL encode the binary string
         url = sys.argv[1] + encoded # Append the encoded binary string to the URL
         response = requests.get(url) # Send a GET request to the URL
-        if binary in response.text: # Check if the binary string is reflected in the response
+        if encoded in response.text: # Check if the binary string is reflected in the response
             print("")
-            print(f"Reflected value found: {binary} with encoding: URL in url: {url} with a length of {len(response.content)}")
+            print(f"Reflected value found: {encoded} with encoding: URL in url: {url} with a length of {len(response.content)}")
         else:
             for enc in ['hex','base64','quopri','uu','rot13','url','ascii']:
                 try:
-                    encoded = eval("urllib.parse."+enc+"(binary)")
+                    dec = int(binary, 2) # convert binary to decimal
+                    char = chr(dec) # convert decimal to utf character
+                    encoded = eval("urllib.parse."+enc+"(char)")
                     url = sys.argv[1] + encoded # Append the encoded binary string to the URL
                     response = requests.get(url) # Send a GET request to the URL
                     if binary in response.text: # Check if the binary string is reflected in the response
@@ -29,7 +33,9 @@ for i in range(256):
                 except:
                     pass
     else:
-        encoded = urllib.parse.quote("11111111") # URL encode the binary string
+        dec = int("11111111", 2) # convert binary to decimal
+        char = chr(dec) # convert decimal to utf character
+        encoded = urllib.parse.quote(char) # URL encode the binary string
         url = sys.argv[1] + encoded # Append the encoded binary string to the URL
         response = requests.get(url) # Send a GET request to the URL
         if binary in response.text: # Check if the binary string is reflected in the response
@@ -38,7 +44,9 @@ for i in range(256):
         else:
             for enc in ['hex','base64','quopri','uu','rot13','url','ascii']:
                 try:
-                    encoded = eval("urllib.parse."+enc+"(binary)")
+                    dec = int("11111111", 2) # convert binary to decimal
+                    char = chr(dec) # convert decimal to utf character
+                    encoded = eval("urllib.parse."+enc+"(char)")
                     url = sys.argv[1] + encoded # Append the encoded binary string to the URL
                     response = requests.get(url) # Send a GET request to the URL
                     if binary in response.text: # Check if the binary string is reflected in the response
